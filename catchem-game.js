c476580935e215001dc0ccb3859e91876fc47c65
kwarchalowski <karol@warchalowski.pl>
2020-03-19 13:10:06 +0100

    counting points and errors

--- catchem-game.js
+++ catchem-game.js
@@ -1,3 +1,4 @@
+// ================ *GAME WINDOW* CONFIG
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -6,7 +7,7 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 300
+                y: 1
             },
             debug: false
         }
@@ -17,37 +18,111 @@ var config = {
         update: update
     }
 };
+// ================ END OF *GAME WINDOW* CONFIG
 
 
 var score = 0;
-var scoreText;
+var text1;
+var text2;
+var text3;
+var pointsCount = 0;
+var errorCount = 0;
+var pointsText;
+var errorText;
+var randomLetter;
+var letters;
+var dropSpeed = 500;
+var letterXpos;
+var lettersList;
+
+
 
 
 var game = new Phaser.Game(config);
 
+
 function preload() {
     // preloading all assets at the begining
-
 }
 
 
 
-
+// ================ *CREATE* FUNCTION
 function create() {
 
     // setting background color
-    this.cameras.main.setBackgroundColor('#123456');
+    this.cameras.main.setBackgroundColor('#142850');
 
-    // setting score text
-    scoreText = this.add.text(10, 580, 'Score: 0', {
+    // UI
+    pointsText = this.add.text(10, 10, 'Points: x', {
         font: "12px Verdana",
-        fill: '#fff'
+        fill: '#AFA'
+    });
+    errorText = this.add.text(10, 30, 'Errors: x', {
+        font: "12px Verdana",
+        fill: '#FAA'
     });
 
 
-}
+    text1 = this.add.text(10, 580, '', {
+        font: "12px Verdana",
+        fill: '#f1f9f9'
+    });
+    text2 = this.add.text(10, 560, '', {
+        font: "12px Verdana",
+        fill: '#f1f9f9'
+    });
+    text3 = this.add.text(10, 540, 'Last input: ', {
+        font: "14px Verdana",
+        fill: '#f1f9f9'
+    });
 
+    letterXpos = Math.floor(Math.random() * 600) + 100;
+
+    // dropping random letter on the scene
+    randomLetter = this.add.text(letterXpos, 0, getRandomLetter().toUpperCase(), {
+        fontSize: '42px',
+        fontFamily: 'Tahoma',
+        fill: '#FFA'
+    });
+
+
+    this.physics.world.enable(randomLetter);
+
+    randomLetter.body.velocity.setTo(0, dropSpeed);
+    randomLetter.body.setMaxVelocity(0, dropSpeed);
+    randomLetter.body.setCollideWorldBounds(true);
+
+
+    // keyboard events (input)
+    this.input.keyboard.on('keydown', function (event) {
+        let uppercaseInput = String.fromCharCode(event.keyCode).toUpperCase();
+        if (event.keyCode >= 48 && event.keyCode <= 90) {
+            (uppercaseInput == randomLetter.text[0]) ? pointsCount++ : errorCount++;
+            text3.setText('Last input: ' + uppercaseInput);
+        }
+    });
+
+}
+// ================ END OF *CREATE* FUNCTION
+
+
+// ================ *UPDATE* FUNCTION
 function update() {
 
-}
+    text2.setText('letter y pos: ' + Math.floor(randomLetter.body.y));
+    pointsText.setText('Points: ' + pointsCount);
+    errorText.setText('Errors: ' + errorCount);
 
+
+}
+// ================ END OF *UPDATE* FUNCTION
+
+
+
+// ================ OTHER METHODS
+function getRandomLetter() {
+    let characters = 'abcdefghijklmnopqrstuvwxyz';
+
+    return characters.charAt(Math.floor(Math.random() * characters.length));
+}
